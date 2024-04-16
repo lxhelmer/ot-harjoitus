b953a306ae3d2412779a5d2b85b9cf2f4f1fdbaf
helmer <helmer.lauronen@gmail.com>
2024-04-16 22:29:02 +0300

    Rework ui and implement adding functionality

--- wol-ctrl/src/ui/device_frame.py
+++ wol-ctrl/src/ui/device_frame.py
@@ -1,22 +1,24 @@
 from tkinter import ttk
 
 class DeviceFrame:
-    def __init__(self, root, deviceRepo):
+    def __init__(self, root, deviceRepo, pos):
         self._root = root
         self._dRepo = deviceRepo
+        self._pos_row = pos[0]
+        self._pos_col = pos[1]
 
     def generate_device_frame(self):
         self._frame = ttk.Frame(self._root)
         for i, dev in enumerate(self._dRepo.find_all()):
-            label = ttk.Label(master=self._frame, text=dev, borderwidth=2, relief="solid")
-            label.grid(row=i, column=0, padx=20,pady=5)
+            device_label = ttk.Label(master=self._frame, text=dev, borderwidth=2, relief="solid")
+            device_label.grid(row=i, column=0, padx=20,pady=5)
             delete_button = ttk.Button(
                     master=self._frame,
-                    text=dev["id"],
+                    text="Delete",
                     command=lambda dev=dev: self.handle_delete(dev["id"]))
 
-            delete_button.grid(row=i, column=1, pady=5)
-        self._frame.pack()
+            delete_button.grid(row=i, column=2, pady=5)
+        self._frame.grid(columnspan = 2, rowspan = 3, row = self._pos_row,column = self._pos_col)
 
     def update(self):
         self._frame.destroy()
--- wol-ctrl/src/ui/ui.py
+++ wol-ctrl/src/ui/ui.py
@@ -7,9 +7,37 @@ class UI:
 
     def __init__(self, root, deviceRepo):
         self._root = root
-        self.device_frame = DeviceFrame(root,deviceRepo)
+        self._device_repo = deviceRepo
+        self._device_frame = DeviceFrame(root,deviceRepo, (0,0))
 
     def start(self):
-        self.device_frame.generate_device_frame()
+        # device frame is placed in class for the updates to work smoothly
+        self._device_frame.generate_device_frame()
+        self._id_label = ttk.Label(master = self._root, text="id")
+        self._name_label = ttk.Label(master = self._root, text="device name")
+        self._user_label = ttk.Label(master = self._root, text="user name")
+        self._add_button = ttk.Button(master = self._root, text="add", command=self._handle_add)
+        self._id_entry = ttk.Entry(master = self._root)
+        self._name_entry = ttk.Entry(master = self._root)
+        self._user_entry = ttk.Entry(master = self._root)
+
+        self._add_label = ttk.Label(master=self._root, text="Add a new device!")
+        self._add_label.grid()
+        self._id_label.grid()
+        self._id_entry.grid(row = 4, column=1, pady = 5)
+        self._name_label.grid()
+        self._name_entry.grid(row=5, column=1, pady = 5)
+        self._user_label.grid()
+        self._user_entry.grid(row=6, column=1, pady = 5)
+        self._add_button.grid(row=6, column=2, pady = 5)
+
+    def _handle_add(self):
+        id = self._id_entry.get()
+        name = self._name_entry.get()
+        user = self._user_entry.get()
+        self._device_repo.create({"id":id,"name":name, "user":user})
+        self._device_frame.update()
+
+
 
 
